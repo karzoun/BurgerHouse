ce97a233195c7a4b755ec37bd7b37b8734a470b8
“karzoun” <karzoun_27@hotmail.com>
2020-10-16 13:17:31 -0700

    working app

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,3 @@
-// var mysql = require("mysql");
-
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "password",
-//     database: "burgers_db"
-// });
-// connection.connect(function(err) {
-//     if (err) {
-//         console.error("error connecting: " + err.stack);
-//         return;
-//     }
-//     console.log("connected as id " + connection.threadId);
-// });
-// module.exports = connection
 const mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
--- models/burger.js
+++ models/burger.js
@@ -1,49 +1,28 @@
-$(function() {
-    $(".create-form").on("submit", function(event) {
-        event.preventDefault();
-
-        var newBurger = {
-            burger_name: $("#newburger")
-                .val()
-                .trim(),
-            devoured: 0
-        };
-
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(function() {
-            console.log("Added new burger");
-            location.reload();
-        });
+var orm = require("../config/orm.js");
+var burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
     });
+  },
 
-    $(".eatburger").on("click", function(event) {
-        event.preventDefault();
-
-        var id = $(this).data("id");
-        var devouredState = {
-            devoured: 1
-        };
-
-        $.ajax("/api/burgers/" + id, {
-            type: "PUT",
-            data: devouredState
-        }).then(function() {
-            console.log("Burger devoured");
-            location.reload();
-        });
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
     });
+  },
 
-    $(".trashburger").on("click", function(event) {
-        event.preventDefault();
-
-        var id = $(this).data("id");
-
-        // Send the DELETE request.
-        $.ajax({
-            type: "DELETE",
-            url: "/api/burgers/" + id
-        }).then(location.reload());
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
     });
-});
+  },
+
+  deleteOne: function(condition, cb) {
+    orm.deleteOne("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
